ca2d0aca50af7adc3c18ce4a41b87d6acbcef218
Bhushan Alhat <alhatbhushan@gmail.com>
2024-09-12 13:16:55 +0530

    [Backend] feat: send email

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,42 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!');
+app.post('/', async (req, res) => {
+	const { to, subject, text } = req.body;
+
+	if (!to || !subject || !text) {
+		return res.status(400).json({
+			error: 'Please provide email, subject, and message body.',
+		});
+	}
+
+	let transporter = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: Number(process.env.SMTP_PORT),
+		secure: true,
+		auth: {
+			user: process.env.SMTP_EMAIL_ADDRESS,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	});
+
+	try {
+		await transporter.sendMail({
+			from: process.env.EMAIL_USER,
+			to,
+			subject,
+			text,
+		});
+
+		res.status(200).json({ message: 'Email sent successfully!' });
+	} catch (error) {
+		console.error('Error sending email:', error);
+		res.status(500).json({ error: 'Failed to send email.' });
+	}
 });
 
 app.listen(3000, () => {
